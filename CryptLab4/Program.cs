49635072b638179429e71a8b14ab8984e04ee3d1
zadolbali10 <anon.yu@mail.ru>
2020-12-16 11:31:18 +0700

    1 task completed

--- CryptLab4/Program.cs
+++ CryptLab4/Program.cs
@@ -1,12 +1,178 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace CryptLab4
 {
    class Program
    {
+      static public string path = "../../../files/";
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         bool repeat = true;
+
+         while (repeat)
+         {
+            Console.WriteLine("Выберете опцию:");
+            Console.WriteLine("<1> Зашифровать файл");
+            Console.WriteLine("<2> Дешифровать файл");
+            Console.WriteLine("<0> Выйти(");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+               case "1":
+               {
+                  Console.WriteLine("Введите название файла для шифрования:");
+                  string inFileName = Console.ReadLine();
+                  byte[] ToEncrypt;
+                  try
+                  {
+                     ToEncrypt = File.ReadAllBytes(path + inFileName);
+                  }
+                  catch (Exception)
+                  {
+                     Console.WriteLine("Нет такого файла!");
+                     break;
+                  }
+
+                  Console.WriteLine("Введите название файла для вывода:");
+                  string outFileName = Console.ReadLine();
+
+                  Console.WriteLine("Введите пароль для шифрования:");
+                  byte[] Password = Encoding.UTF8.GetBytes(Console.ReadLine());
+
+                  var hashAlg = GetHashAlgorithm();
+                  File.WriteAllBytes(path + outFileName, Encrypt(ToEncrypt, Password, hashAlg));
+
+                  Console.WriteLine("Шифрование прошло успешно!");
+                  break;
+               }
+               case "2":
+               {
+                  Console.WriteLine("Введите название файла для дешифровки:");
+                  string inFileName = Console.ReadLine();
+                  byte[] ToDecrypt;
+                  try
+                  {
+                     ToDecrypt = File.ReadAllBytes(path + inFileName);
+                  }
+                  catch (Exception)
+                  {
+                     Console.WriteLine("Нет такого файла!");
+                     break;
+                  }
+
+                  Console.WriteLine("Введите название файла для вывода:");
+                  string outFileName = Console.ReadLine();
+
+                  Console.WriteLine("Введите пароль для дешифровки:");
+                  byte[] Password = Encoding.UTF8.GetBytes(Console.ReadLine());
+
+                  var hashAlg = GetHashAlgorithm();
+                  File.WriteAllBytes(path + outFileName, Decrypt(ToDecrypt, Password, hashAlg));
+
+                  Console.WriteLine("Шифрование прошло успешно!");
+                  break;
+               }
+               case "0":
+               {
+                  repeat = false;
+                  break;
+               }
+               default:
+               {
+                  Console.WriteLine("Неверный выбор!");
+                  break;
+               }
+            }
+         }
+      }
+
+      public static HashAlgorithm GetHashAlgorithm()
+      {
+         bool repeat = true;
+         HashAlgorithm res = HashAlgorithm.Create("SHA-256");
+
+         while (repeat)
+         {
+            repeat = false;
+            Console.WriteLine("Выберете алгоритм генерации хэша:");
+            Console.WriteLine("<1> SHA-256");
+            Console.WriteLine("<2> MD5");
+            Console.WriteLine("<3> SHA1");
+
+            string hashChoice = Console.ReadLine();
+
+            switch (hashChoice)
+            {
+               case "1":
+               {
+                  res = HashAlgorithm.Create("SHA-256");
+                  break;
+               }
+               case "2":
+               {
+                  res = HashAlgorithm.Create("MD5");
+                  break;
+               }
+               case "3":
+               {
+                  res = HashAlgorithm.Create("SHA1");
+                  break;
+               }
+               default:
+               {
+                  repeat = true;
+                  Console.WriteLine("Неверный выбор!");
+                  break;
+               }
+            }
+         }
+         return res;
+      }
+
+      public static byte[] Encrypt(byte[] dataToEncrypt, byte[] password, HashAlgorithm hashAlg)
+      {
+         using (var aes = Aes.Create())
+         {
+            aes.Mode = CipherMode.CBC;
+            aes.Key = hashAlg.ComputeHash(password);
+            aes.IV = aes.Key.ToList().Take(16).ToArray();
+            aes.Padding = PaddingMode.PKCS7;
+
+            return PerformCrypt(aes.CreateEncryptor(), dataToEncrypt);
+         }
+      }
+
+      public static byte[] Decrypt(byte[] dataToDecrypt, byte[] password, HashAlgorithm hashAlg)
+      {
+         using (var aes = Aes.Create())
+         {
+            aes.Mode = CipherMode.CBC;
+            aes.Key = hashAlg.ComputeHash(password);
+            aes.IV = aes.Key.ToList().Take(16).ToArray();
+            aes.Padding = PaddingMode.PKCS7;
+            var decryptor = aes.CreateDecryptor();
+
+            return PerformCrypt(aes.CreateDecryptor(), dataToDecrypt);
+         }   
+      }
+
+      public static byte[] PerformCrypt(ICryptoTransform transform, byte[] data)
+      {
+         using (var msDecrypt = new MemoryStream())
+         {
+            using (var csEncrypt = new CryptoStream(msDecrypt, transform, CryptoStreamMode.Write))
+            {
+               csEncrypt.Write(data, 0, data.Length);
+               csEncrypt.FlushFinalBlock();
+               return msDecrypt.ToArray();
+            }
+         }
       }
    }
 }
